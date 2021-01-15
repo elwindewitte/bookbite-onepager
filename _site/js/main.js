5e01a3275f31cb85a56e0b67188acf9b13958086
Elwin <elwindewitte@gmail.com>
2021-01-15 07:42:39 +0100

    Changes to menu

--- _site/js/main.js
+++ _site/js/main.js
@@ -2,10 +2,33 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navigation").style.top = "0";
-  } else {
-    document.getElementById("navigation").style.top = "-89px";
+  var menuOpen = localStorage.getItem('menuOpen');
+  if (menuOpen !== 'true') {
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navigation").style.top = "0";
+    } else {
+      document.getElementById("navigation").style.top = "-89px";
+    }
   }
   prevScrollpos = currentScrollPos;
+}
+
+function toggleMenu() {
+  var menuOpen = localStorage.getItem('menuOpen');
+  var menuTrigger = document.getElementById('menu-trigger');
+  var menu = document.getElementById('menu');
+
+  if (!(menuOpen == 'true') && !(menuOpen == 'false')) {
+    menu.classList.add('visible');
+    menu.classList.remove('hidden');
+    localStorage.setItem('menuOpen', 'true');
+  } else if (menuOpen == 'true') {
+    menu.classList.add('hidden');
+    menu.classList.remove('visible');
+    localStorage.setItem('menuOpen', 'false');
+  } else if (menuOpen == 'false') {
+    menu.classList.add('visible');
+    menu.classList.remove('hidden');
+    localStorage.setItem('menuOpen', 'true');
+  }
 }
